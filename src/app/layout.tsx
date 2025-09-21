1e97159a84555977c2cc2a680e33d6fed3b23a06
perekrest1 <perekrestb@gmail.com>
2025-09-21 22:38:46 +0700

    Remove old files

--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: 'LUCH-IT Dashboard' };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ru">
-      <body>{children}</body>
-    </html>
-  );
-}
