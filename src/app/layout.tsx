bf0a1a73cadbb4040b05874bebfb945224433cd6
perekrest1 <perekrestb@gmail.com>
2025-09-21 17:55:40 +0700

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: 'LUCH-IT Dashboard' };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
